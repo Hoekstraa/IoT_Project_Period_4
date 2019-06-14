a8e8ba577e85e0b442ab2999cb6b3d38d0ceebdf
Altlock <altlockg@gmail.com>
2019-06-14 20:12:42 +0200

    Add C# library for connection to Arduino and a Proof Of Concept app

---
+++ CCPOC/CCPOC/CCPOC/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CCPOC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void B1_OnPressed(object sender, EventArgs e)
+        {
+            L1.Text = CC.Conn.Get("temp1");
+        }
+
+        private void B2_OnPressed(object sender, EventArgs e)
+        {
+            L2.Text = CC.Conn.Set("temp1", 5);
+        }
+
+        private void B3_OnPressed(object sender, EventArgs e)
+        {
+            L3.Text = CC.Conn.Set("temp1", 9001);
+        }
+    }
+}
---
+++ ConnectionClient/ConnectionClient/Conn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CC
+{
+    public class Conn
+    {
+        public static string Get(string thing)
+        {
+            return SendString(string.Concat("get ", thing));
+        }
+
+        public static string Set(string thing, int value)
+        {
+            return SendString(string.Concat("set ", thing, " ", value));
+        }
+        
+        /// <summary>
+        /// Send a string to the server.
+        /// </summary>
+        /// <param name="s">The string to send.</param>
+        /// <returns>The response of the server.</returns>
+        private static string SendString(string s)
+        {
+            var bytes = new byte[1024];
+
+            var ipAddress = IPAddress.Parse("192.168.1.2");
+            var remoteEndPoint = new IPEndPoint(ipAddress,2525);
+
+            var sender = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            var msg = Encoding.ASCII.GetBytes(s);
+
+            try
+            {
+                sender.Connect(remoteEndPoint);
+                var bytesSent = sender.Send(msg);
+
+                var bytesReceived = sender.Receive(bytes);
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+                return Encoding.ASCII.GetString(bytes, 0, bytesReceived);
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
