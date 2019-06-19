8bc44ea57159f3e1490bce0d786ab7ec877ff9b7
Altlock <altlockg@gmail.com>
2019-06-19 15:08:42 +0200

    Add feature to turn off actuators

--- Connection_Library/sensorFunctions.ino
+++
@@ -1,5 +0,0 @@
-String humidity(){return "humidity";}
-String waterpump(){return "waterpump";}
-String groundhumidity(){return "groundhumidity";}
-String lamp(){return "lamp";}
-String fan(){return "fan";}
